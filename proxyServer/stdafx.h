3af16fbc4f59f415cdf5122194e2337782fb7d1b
hjw21century <hjw21century@163.com>
2017-07-11 00:18:23 +0800

    项目代码

---
+++ proxyServer/stdafx.h
@@ -0,0 +1,19 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#include <stdio.h>
+#include <tchar.h>
+
+#pragma comment(lib,"WSock32.Lib")
+#pragma comment(lib,"../debug/BasicUsageEnvironment.lib")
+#pragma comment(lib,"../debug/groupsock.lib")
+#pragma comment(lib,"../debug/liveMedia.lib")
+#pragma comment(lib,"../debug/UsageEnvironment.lib")
+
+// TODO:  在此处引用程序需要的其他头文件
---
+++ testProgs/testProgs.cpp
@@ -0,0 +1,11 @@
+// testProgs.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+    return 0;
+}
+
